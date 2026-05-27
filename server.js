62034e3f08888f1b8f0aa524f24e4d6bf4955fd4
esancheza-eng <esancheza@unemi.edu.ec>
2026-05-27 17:02:26 -0500

    Set default port to 3000 if not specified

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ app.post("/webhook", (req, res) => {
   return res.sendStatus(200);
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
